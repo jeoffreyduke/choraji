035ff845a840db0caeb65ffd569bfdb18acdcb67
jeoffreyduke <jeoffreyduke@gmail.com>
2022-09-15 00:07:08 +0100

    additions of images

--- components/Homecomp.tsx
+++ components/Homecomp.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 import styles from "../styles/Home.module.css";
 
@@ -21,6 +22,14 @@ const Homecomp = () => {
               <a href="#" className={styles.carousel__item__content__button}>
                 Read more
               </a>
+              <div className={styles.carousel__item__content__image}>
+                <Image
+                  src="https://images.unsplash.com/photo-1617675460003-8b8b0b0b1b1a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+                  alt="image"
+                  width={500}
+                  height={500}
+                />
+              </div>
             </div>
           </div>
         </div>
